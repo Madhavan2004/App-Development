32092d115a63182fb38515d2f14fbbf0a477cf38
Madhavan2004 <120085161+Madhavan2004@users.noreply.github.com>
2023-10-11 12:12:21 +0530

    Delete 17 directory

--- 17/src/main/java/com/crudoperation/FurnitureCRUD/Repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.crudoperation.FurnitureCRUD.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.crudoperation.FurnitureCRUD.Entity.UserEntity;
-
-public interface UserRepository extends JpaRepository<UserEntity, Long>{
-
-}
